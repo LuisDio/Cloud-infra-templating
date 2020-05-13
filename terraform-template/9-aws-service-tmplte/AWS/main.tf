e9ac52dc0b9938bbd941ce381388d7180cb4891f
LuisDio <luis.diolindo@gmail.com>
2020-05-13 18:22:44 +0300

    add compute module to root

--- terraform-template/9-aws-service-tmplte/AWS/main.tf
+++ terraform-template/9-aws-service-tmplte/AWS/main.tf
@@ -15,3 +15,16 @@ module "networking" {
   public_cidrs = "${var.public_cidrs}"
   accessip     = "${var.accessip}"
 }
+
+# Deploy compute resource
+module "compute" {
+  source = "./compute"
+  instance_count = "${var.instance_count}"
+  key_name = "${var.key_name}"
+  public_key_path = "${var.public_key_path}"
+  instance_type = "${var.server_instance_type}"
+  subnets = "${module.networking.public_subnets}"   # These are output from module networking
+  security_group = "${module.networking.public_sg}" # These are output from module networking
+  subnet_ips = "${module.networking.subnet_ips}"    # These are output from module networking
+  
+}
--- terraform-template/9-aws-service-tmplte/AWS/outputs.tf
+++ terraform-template/9-aws-service-tmplte/AWS/outputs.tf
@@ -4,3 +4,34 @@
 output "bucket_name" {
   value = "${module.storage.bucket_name}"
 }
+
+#---Networking Output-------
+output "public Subnets" {
+  value = "${join(",", module.networking.public_subnets)}"
+}
+
+output "Subnet IPs" {
+  value = "${join(",", module.networking.Subnet_ips)}"
+}
+
+output "Public Security Group" {
+  value = "${module.networking.public_sg}"
+}
+
+#---Compute Output---------
+output "Public Instance IDs" {
+  value = "${module.compute.server_id}"
+}
+
+output "Public Instance IPs" {
+  value = "${module.compute.server_ip}"
+}
+
+output "Public Security Group" {
+  value = "${module.networking.public_sg}"
+}
+
+
+
+
+
